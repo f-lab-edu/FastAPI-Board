2956abf5103677382a62746d6c8a78fabcb32e75
Dongyeon1201 <dongyeon1201@gmail.com>
2024-06-30 19:08:17 +0900

    단일 게시글의 조회/수정/삭제 시, 게시글 데이터 존재 여부 및 작성자 확인 로직을 데코레이터로 이관

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import secrets
 from datetime import datetime
+from functools import wraps
+from typing import Callable
 from uuid import UUID
 
 from fastapi import FastAPI, HTTPException, status
@@ -12,6 +14,27 @@ app = FastAPI()
 post_data = {}
 
 SECRETS_KEY_LENGTH = 32
+
+
+# 작성자 인증
+def verify_author(request: Request, post_id: UUID):
+    post = post_data.get(post_id)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="존재하지 않는 게시글입니다.")
+
+    if request.cookies.get("token") != post.token:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="작성자만 접근 가능합니다.")
+
+
+# 데코레이터 정의
+def author_verification(func: Callable):
+    @wraps(func)
+    def wrapper(request: Request, post_id: UUID, *args, **kwargs):
+        verify_author(request, post_id)
+        return func(request, post_id, *args, **kwargs)
+
+    return wrapper
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
@@ -42,22 +65,14 @@ def read_posts() -> list[ResponsePost]:
     response_model=ResponsePost,
     status_code=status.HTTP_200_OK
 )
+@author_verification
 def read_post(request: Request, post_id: UUID) -> ResponsePost:
     """
     게시글 조회
     :param post_id: 조회할 게시글의 ID
     :return: 특정 ID를 가진 게시글의 내용을 반환합니다. 존재하지 않는 게시글 ID인 경우 404 에러를 발생시킵니다.
     """
-    try:
-        post = post_data[post_id]
-
-        # 게시글 작성자인지 확인
-        if request.cookies.get("token") != post.token:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="작성자만 접근 가능합니다.")
-
-        return post
-    except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="존재하지 않는 게시글입니다.")
+    return post_data[post_id]
 
 
 @app.post("/posts/", response_model=ResponsePost, status_code=status.HTTP_201_CREATED)
@@ -80,51 +95,40 @@ def create_post(request: Request, post: CreatePost) -> ResponsePost:
     response_model=ResponsePost,
     status_code=status.HTTP_200_OK
 )
+@author_verification
 def update_post(request: Request, post_id: UUID, update_data: UpdatePost) -> ResponsePost:
     """
     게시글 수정
     :param post_id: 수정할 게시글의 ID
+    :param update_data: 수정할 게시글의 내용 (author, title, content)
     :return: 특정 ID를 가진 게시글의 내용을 수정합니다. 존재하지 않는 게시글 ID인 경우 404 에러를 발생시킵니다.
     """
-    try:
-        post = post_data[post_id]
 
-        # 게시글 작성자인지 확인
-        if request.cookies.get("token") != post.token:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="작성자만 변경 가능합니다.")
+    # 수정을 위해 게시글 데이터 가져오기
+    post = post_data[post_id]
 
-        # 게시글 내용 수정
-        for key, value in update_data:
-            if value:
-                setattr(post, key, value)
+    # 게시글 내용 수정
+    for key, value in update_data:
+        if value:
+            setattr(post, key, value)
 
-        # 업데이트 시간 설정
-        post.updated_at = datetime.utcnow()
+    # 업데이트 시간 재설정
+    post.updated_at = datetime.utcnow()
 
-        return post
-    except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="존재하지 않는 게시글입니다.")
+    return post
 
 
 @app.delete(
     "/posts/{post_id}",
     status_code=status.HTTP_200_OK
 )
+@author_verification
 def delete_post(request: Request, post_id: UUID) -> dict[str, str]:
     """
     게시글 삭제
     :param post_id: 삭제할 게시글의 ID
     :return: 특정 ID를 가진 게시글을 삭제합니다. 존재하지 않는 게시글 ID인 경우 404 에러를 발생시킵니다.
     """
-    try:
-        post = post_data[post_id]
+    del post_data[post_id]
 
-        # 게시글 작성자인지 확인
-        if request.cookies.get("token") != post.token:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="작성자만 접근 가능합니다.")
-
-        del post_data[post_id]
-
-        return {"message": "게시글이 삭제되었습니다."}
-    except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="존재하지 않는 게시글입니다.")
+    return {"message": "게시글이 삭제되었습니다."}
