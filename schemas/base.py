54b148b21eafe1813e2fc94458ceff471140f807
Dongyeon1201 <dongyeon1201@gmail.com>
2024-06-30 19:24:29 +0900

    `datetime.utcnow` 만 사용 시, Timezone이 설정되지 않은 `naive datetime` 로 적용되는 것을 확인 후 수정

--- schemas/base.py
+++ schemas/base.py
@@ -1,8 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Annotated
 from uuid import UUID, uuid4
 
 from pydantic import Field, BaseModel
+
+
+def _default_time():
+    return datetime.now(timezone.utc)
 
 
 def _change_local_time(time: datetime):
@@ -11,8 +15,8 @@ def _change_local_time(time: datetime):
 
 class BaseDataInfo(BaseModel):
     id: UUID = Field(default_factory=uuid4)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=_default_time)
+    updated_at: datetime = Field(default_factory=_default_time)
 
 
 class AuthMixin(BaseModel):
